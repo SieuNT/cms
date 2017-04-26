c02985fc8b6a7cdbe5178644f119bacde9dfb332
vagrant <vagrant@cms.dev>
2017-04-26 08:08:28 +0000

    .

---
+++ cms/frontend/assets/AppAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot/themes';
+    public $baseUrl = '@web/themes';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
